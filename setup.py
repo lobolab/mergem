92728b368121e026c52118a068004939742fe5dd
Archana Hari <archh1@umbc.edu>
2021-12-14 12:12:30 -0500

    mergem v0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = '0.1'
+version = '0.2'
 
 setup(name='mergem',
       version=version,
